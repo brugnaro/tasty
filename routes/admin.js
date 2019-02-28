f28255b8c9654cbaf46cb7eb0b57c725d8d202b2
marcos <marcosrodriguesbrugnaro@gmail.com>
2019-02-28 18:57:34 -0300

    Begin login autentication

--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var users = require('./../inc/users');
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
@@ -9,11 +10,33 @@ router.get('/', function (req, res, next) {
 
 });
 
+router.post('/login', function (req, res, next) {
+
+  if (!req.body.email) {
+    users.render(req, res, 'Preencha o campo email');
+  } else if (!req.body.password) {
+    users.render(req, res, 'Preencha o campo senha');
+  } else {
+
+    users.login(req.body.email, req.body.password).then(user => {
+
+      req.session.user = user;
+
+      res.redirect('/admin');
+
+    }).catch(err => {
+
+      users.render(req, res, err.message || err);
+
+    });
+
+  }
+
+});
+
 router.get('/login', function (req, res, next) {
 
-  res.render('admin/login', {
-
-  });
+  users.render(req, res, null);
 
 });
 
